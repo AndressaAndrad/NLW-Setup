e0b5c5a3a6ace1574867c750621fd3ddecd206e6
Andressa Andrade <aandrade@psgtecnologia.com.br>
2023-01-20 09:15:43 -0400

    Termino de NLW

--- server/prisma/migrations/20230119124739_create_the_rest_of_the_tables/migration.sql
+++ server/prisma/migrations/20230119124739_create_the_rest_of_the_tables/migration.sql
@@ -1,5 +1,5 @@
 -- CreateTable
-CREATE TABLE "habit_week_days" (
+CREATE TABLE "habits_week_days" (
     "id" TEXT NOT NULL PRIMARY KEY,
     "habit_id" TEXT NOT NULL,
     "week_day" INTEGER NOT NULL
@@ -19,10 +19,10 @@ CREATE TABLE "day_habits" (
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "habit_week_days_habit_id_week_day_key" ON "habit_week_days"("habit_id", "week_day");
+CREATE UNIQUE INDEX "habits_week_days_habit_id_week_day_key" ON "habits_week_days"("habit_id", "week_day");
 
 -- CreateIndex
-CREATE INDEX "days_date_idx" ON "days"("date");
+CREATE UNIQUE INDEX "days_date_key" ON "days"("date");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "day_habits_day_id_habit_id_key" ON "day_habits"("day_id", "habit_id");
